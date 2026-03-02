8027e48f983fb3bf11f52ede1f00837b42e512de
Patricia <112484371+Triyona@users.noreply.github.com>
2026-03-02 17:13:30 +0300

    fake fix for steps button

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded',() => {
     
     const progress = document.querySelector('.progress');
     const stepIndicators = document.querySelectorAll('.progress-container li');
-    const firstButton = document.querySelector('.first-btn');
+    const prevButton = document.querySelector('.prev-btn');
     const nextButton = document.querySelector('.next-btn');
     const submitButton = document.querySelector('.submit-btn');
 
@@ -10,45 +10,32 @@ document.addEventListener('DOMContentLoaded',() => {
 
     let currentStep = 0;
 
-    const updateButtons = () => {
-        firstButton.style.display  = currentStep === 0 ? 'inline-block' : 'none';
-        nextButton.style.display   = currentStep === 1 ? 'inline-block' : 'none';
-        submitButton.style.display = currentStep === 2 ? 'inline-block' : 'none';
-    };
-
     const updateProgress = () => {
         let width = currentStep / (stepIndicators.length - 1);
         progress.style.transform = `scaleX(${width})`;
 
         stepIndicators.forEach((indicator, index) => {
-            indicator.classList.toggle("current", currentStep === index)
-            indicator.classList.toggle("done", currentStep > index)
-        })
+            indicator.classList.toggle("current", currentStep === index);
+            indicator.classList.toggle("done", currentStep > index);
+        });
+    };
 
-        updateButtons();
-    }
+    prevButton.addEventListener("click", (e) => {
+        e.preventDefault(); //prevent form submission
 
-    //*event listeners
-
-    firstButton.addEventListener('click', () => {
-        currentStep++;
-        updateProgress();
+        if (currentStep < 2){
+            currentStep++;
+            updateProgress();
+        }
     });
 
-    nextButton.addEventListener('click', () => {
-        currentStep++;
-        updateProgress();
+    nextButton.addEventListener("click", (e) => {
+        e.preventDefault(); //prevent form submission
+
+        if (currentStep < 3){
+            currentStep++;
+            updateProgress();
+        }
     });
 
-    submitButton.addEventListener('click', () => {
-    currentStep = 0;     // reset to step 1
-    updateProgress();    // update progress bar + buttons
-    });
-
-    // setInterval(() => {
-    //     currentStep++;
-    //     if(currentStep > stepIndicators.length - 1){
-    //         currentStep = 0
-    //     }
-    // }, 2000);
 });
